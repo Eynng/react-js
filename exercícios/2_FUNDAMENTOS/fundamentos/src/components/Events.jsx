b6d68e814557bbb6cb3332a606db410557c4a7f0
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-10-25 11:37:44 -0300

    Adicionando Eventos - React JS - HoraDeCodar

--- exercícios/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ exercícios/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 const Events = () => {
+
+  const handleClick = (e) => {
+    console.log(e)
+    console.log("Executou")
+  }
+
+
+
+
   return (
     <div>
       <div>
         <button onClick={() => console.log('Testando função')}>Clique aqui</button>
+      </div>
+      {/* 7 - Evento da função */}
+      <div>
+        <button onClick={handleClick}>Clique aqui com função!</button>
       </div>
     </div>
   );
