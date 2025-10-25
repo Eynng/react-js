ad9105dbf949ab62ec0f090c43654f5c936a3b8b
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-10-25 13:12:59 -0300

    Utilização do useState na prática / Exemplos - React JS - HoraDeCodar

--- exercícios/3_AVANCANDO_EM_REACT/avançando/src/App.jsx
+++ exercícios/3_AVANCANDO_EM_REACT/avançando/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+// 3 - imagem em assets
 import night from "./assets/night.jpg";
+// 4 - useState
+import Data from "./components/Data";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ paddingBottom: "500px" }}>
       <h1>Avançando em React</h1>
       {/* 1 - Imagem em public */}
       <img src="/img.jpg" alt="Alguma imagem" />
@@ -11,6 +14,8 @@ function App() {
       <img src="./src/assets/night.jpg" alt=" alguma imagem" />
       {/* 3 - imagem em assets com require */}
       <img src={night} alt="" />
+      {/* 4 - useState */}
+      <Data />
     </div>
   );
 }
