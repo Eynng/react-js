2587c2c968a27d6fc19a10c63fa36d4e0148fc28
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-10-25 11:47:30 -0300

    Função de renderização - React JS - HoraDeCodar

--- exercícios/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ exercícios/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
 const Events = () => {
-
   const handleClick = (e) => {
-    console.log(e)
-    console.log("Executou")
-  }
+    console.log(e);
+    console.log("Executou");
+  };
 
+  // 8 - Função e renderização
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Renderizando isso</h1>;
+    } else {
+      return <h1>Renderizando outra coisa</h1>;
+    }
+  };
 
-
+  // return 10 > 2 && <p>Carregando....</p>
 
   return (
     <div>
       <div>
-        <button onClick={() => console.log('Testando função')}>Clique aqui</button>
+        <button onClick={() => console.log("Testando função")}>
+          Clique aqui
+        </button>
       </div>
       {/* 7 - Evento da função */}
       <div>
         <button onClick={handleClick}>Clique aqui com função!</button>
       </div>
+      {/* 8 - Função com render */}
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   );
 };
